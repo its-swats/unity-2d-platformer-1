bca88375226ce54bc45d0d5d124f3808d2c19bcf
Shawn Watson <shawnw86@gmail.com>
2019-07-05 15:44:48 -0700

    camera, level assets

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -52,8 +52,8 @@ public class PlayerController : MonoBehaviour
 
         if(horizontal != 0 && (!ducking || !isGrounded)){
             rb2d.velocity = new Vector2(horizontal * playerSpeed, rb2d.velocity.y);
-        } else if(ducking && isGrounded){
-            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+        // } else if(ducking && isGrounded){
+        //     rb2d.velocity = new Vector2(0, rb2d.velocity.y);
         } else {
             rb2d.velocity = new Vector2(0, rb2d.velocity.y);
         }
@@ -86,10 +86,9 @@ public class PlayerController : MonoBehaviour
     }
 
     private bool CheckGround(){
-        Debug.DrawRay(transform.position, new Vector2(0, 0.02f), Color.green);
         RaycastHit2D hitOne = Physics2D.Raycast(groundCheck.transform.position, Vector2.down, 0.02f, groundLayer);
         RaycastHit2D hitTwo = Physics2D.Raycast(groundCheck2.transform.position, Vector2.down, 0.02f, groundLayer);
-        if (hitOne.collider != null || hitTwo.collider != null){
+        if ((hitOne.collider != null || hitTwo.collider != null) && rb2d.velocity.y == 0 ){
             return true;
         }        
         return false;
@@ -102,15 +101,15 @@ public class PlayerController : MonoBehaviour
     }
 
     private void UpdateHitboxes(){
-        if(animator.GetCurrentAnimatorStateInfo(0).IsName("Duck")){
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("Duck") && !duckingCollider.enabled ){
             duckingCollider.enabled = true;
             jumpingCollider.enabled = false;
             standingCollider.enabled = false;
-        } else if(animator.GetCurrentAnimatorStateInfo(0).IsName("Jump")){
+        } else if(animator.GetCurrentAnimatorStateInfo(0).IsName("Jump") && !jumpingCollider.enabled){
             duckingCollider.enabled = false;
             jumpingCollider.enabled = true;
             standingCollider.enabled = false;
-        } else {
+        } else if (!standingCollider.enabled) {
             duckingCollider.enabled = false;
             jumpingCollider.enabled = false;
             standingCollider.enabled = true;
