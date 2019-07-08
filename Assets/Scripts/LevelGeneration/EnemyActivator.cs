5c0fd0ba180b3063652ca1ff6377613ff44ec4d6
Shawn Watson <shawnw86@gmail.com>
2019-07-07 18:11:06 -0700

    structure change, start adding enemy spawns

---
+++ Assets/Scripts/LevelGeneration/EnemyActivator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyActivator : MonoBehaviour
+{
+    [SerializeField] private LayerMask whatToTrigger;
+
+    void Update()
+    {
+           RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 10f, whatToTrigger);
+           if(hit.collider != null){
+               hit.collider.GetComponent<EnemySpawner>().SpawnEnemy();
+           }
+    }
+}
---
+++ Assets/Scripts/LevelGeneration/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] enemyArray;
+    private bool active = true;
+
+    public void SpawnEnemy(){
+        if(active){
+            int enemyIdx = Random.Range(0, enemyArray.Length);
+            Instantiate(enemyArray[enemyIdx], transform.position, transform.rotation);
+            active = false;
+        }
+    }
+}
