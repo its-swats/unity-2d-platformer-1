2b15a3f79b279d01f3c262c84e32922a07ba33c8
Shawn Watson <shawnw86@gmail.com>
2019-07-15 19:49:59 -0700

    particle ffects on death

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,10 +6,16 @@ public class Health : MonoBehaviour
 {
     [SerializeField] private float health;
     [SerializeField] private bool isPlayer;
+    [SerializeField] private ParticleSystem deathEffect;
+    [SerializeField] private float deathDelay = 0f;
+    [SerializeField] private Vector2 deathTrajectory;
+
+    private bool dying = false;
  
     public void Hit(float damage){
         health -= damage;
-        if(health <= 0){
+        if(health <= 0 && !dying){
+            dying = true;
             DestroySelf();
         }
     }
@@ -18,8 +24,19 @@ public class Health : MonoBehaviour
         if(isPlayer){
             StartCoroutine(GetComponent<PlayerController>().Die());
         } else {
-            Destroy(gameObject);
+            if(deathTrajectory != Vector2.zero){
+                GetComponent<Rigidbody2D>().velocity = deathTrajectory;
+            }
+            if(deathEffect){
+                Instantiate(deathEffect, GetComponent<Renderer>().bounds.center, transform.rotation).Play();
+            }
+            StartCoroutine(Die());
         }
+    }
+
+    IEnumerator Die(){
+        yield return new WaitForSeconds(deathDelay);
+        Destroy(gameObject);
     }
 
 }
